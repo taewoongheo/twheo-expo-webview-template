2b6fcc8f356882ac868cfeac09ae1d7d40ffb259
taewoongheo <chamjoeun0111@gmail.com>
2025-12-03 19:53:53 +0900

    refactor(image): remove cssInterop usage

--- apps/expo/src/components/ui/image.tsx
+++ apps/expo/src/components/ui/image.tsx
@@ -1,18 +1,14 @@
 import type { ImageProps } from 'expo-image';
 import { Image as NImage } from 'expo-image';
-import { cssInterop } from 'nativewind';
 
 interface Props extends ImageProps {
   className?: string;
 }
 
-cssInterop(NImage, { className: 'style' });
-
-export default function Image({ style, placeholder, className, ...props }: Props) {
-  return <NImage style={style} placeholder={placeholder} className={className} {...props} />;
+export default function Image({ placeholder, className, ...props }: Props) {
+  return <NImage placeholder={placeholder} className={className} {...props} />;
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const preloadImages = (sources: string[]) => {
   NImage.prefetch(sources);
 };
